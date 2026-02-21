05841d03860ff9f9db03b45f99f82336c437e576
dixma <dixma@Dix>
2026-02-21 02:16:27 -0500

    ya lo conecte a una database remota

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -7,19 +7,20 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     private static final String URL =
-        "jdbc:sqlserver://26.110.90.173:1433;"
-      + "databaseName=BDHospitalCibertec;"
+        "jdbc:sqlserver://proyectodixma-sql.database.windows.net:1433;"
+      + "database=db-proyecto;"
       + "encrypt=true;"
-      + "trustServerCertificate=true;"
-      + "loginTimeout=5;";
+      + "trustServerCertificate=false;"
+      + "hostNameInCertificate=*.database.windows.net;"
+      + "loginTimeout=30;";
 
-    private static final String USER = "sa";
-    private static final String PASSWORD = "Jdhhfj_12";
+    private static final String USER = "dixma@proyectodixma-sql";
+    private static final String PASSWORD = "Jdhhfj_12"; // tu password real
 
     public static Connection conectar() {
         try {
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println(">>> Conectado a BDHospitalCibertec");
+            System.out.println(">>> Conectado a Azure db-proyecto 🚀");
             return conn;
         } catch (SQLException e) {
             e.printStackTrace();
