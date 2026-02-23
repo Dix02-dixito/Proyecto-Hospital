fbf608f64588fcc416c0fc9745a2b6688df97422
dixma <dixma@Dix>
2026-02-23 17:55:59 -0500

    a

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -7,33 +7,30 @@ import conexion.ConexionBD;
 public class UsuarioDAO {
 
     public static String validarLogin(String usuario, String contraseña) {
+    	String rol = null;
+    	try {
+    		Connection cn = ConexionBD.conectar();
+    		String sql = "SELECT rol_usuario FROM usuario " +
+    		             "WHERE nombre_usuario = ?" +
+    				     "AND contrasena_usuario = ?" +
+    		             "AND estado_usuario + 1";
+    		
+    		PreparedStatement ps = cn.prepareStatement(sql);
+    		ps.setString(1, usuario);
+    		ps.setString(2, contraseña);
+    		
+    		ResultSet rs = ps.executeQuery();
+    		
+    		if(rs.next()) {
+    			rol = rs.getString("rol_usuario");
+    			
+    		}
+    		cn.close();
+    		
+    	}catch (Exception e){
+    		JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
 
-        String rol = null;
-
-        try {
-        	Connection cn = ConexionBD.conectar();
-
-            String sql = "SELECT rol_usuario FROM usuario " +
-                         "WHERE nombre_usuario = ? " +
-                         "AND contrasena_usuario = ? " +
-                         "AND estado_usuario = 1";
-
-            PreparedStatement ps = cn.prepareStatement(sql);
-            ps.setString(1, usuario);
-            ps.setString(2, contraseña);
-
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                rol = rs.getString("rol_usuario");
-            }
-
-            cn.close();
-
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
-        }
-
-        return rol;
+    	}
+    	return rol;
     }
 }
